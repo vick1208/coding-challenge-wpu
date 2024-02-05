0b6dc727b44d87e4262c4d68b32c9701f8d0d133
Vicky AS <vickyhuang808@gmail.com>
2024-02-05 11:00:59 +0700

    hari ke-8

---
+++ 8_countsheep.js
@@ -0,0 +1,12 @@
+var countSheep = function (num){
+    // let res = '';
+
+    // for(let i = 1; i <= num;i++){
+    //     res += `${i} sheep...`;
+    // }
+    // return res;
+
+    return [...Array(num)].map((_, i)=>`${i+1} sheep...`).join('');
+  }
+
+  console.log(countSheep(3))
