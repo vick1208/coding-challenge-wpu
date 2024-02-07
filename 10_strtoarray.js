c5c6f856a0ad44e58ebf1bb72ed492cce480f685
Vicky AS <vickyhuang808@gmail.com>
2024-02-07 20:44:48 +0700

    day 10/366

---
+++ 10_strtoarray.js
@@ -0,0 +1,19 @@
+
+// Hari ke-10
+// String to Array
+
+
+
+// function stringToArray(string){
+//     return string.split(' ');
+// }
+
+const stringToArray = (string) => string.split(' ');
+
+
+// cetak hasil test
+console.log(stringToArray("Hometown Cha Cha Cha"));
+console.log(stringToArray("Harimau Macan Tikus Naga"));
+console.log(stringToArray("Reog Kecak"));
+console.log(stringToArray("Kosong Satu Sembilan"));
+
