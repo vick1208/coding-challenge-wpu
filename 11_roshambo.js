8b30213d4b3511da642daddd01eb6c1e1e5adf0c
Vicky AS <vickyhuang808@gmail.com>
2024-02-11 23:42:47 +0700

    day 11/366

---
+++ 11_roshambo.js
@@ -0,0 +1,28 @@
+// Hari ke-11
+// Roshambo
+
+
+// const rps = (p1, p2) => {
+//     if (p1 === p2) {return "Draw!"} 
+//     else if (p1 === 'rock' && p2 === 'scissors' || p1 === 'scissors' && p2 === 'paper' || p1 === 'paper' && p2 === 'rock') {
+//         return 'Player 1 won!';
+//     } else {
+//         return 'Player 2 won!'
+//     }
+// };
+const rps = (p1, p2) => {
+    const rules = {
+        rock: 'scissors',
+        paper: 'rock',
+        scissors: 'paper',
+    };
+    // if (p1 === p2) {
+    //     return 'Draw!';
+    // } else {
+    //     return `Player ${rules[p1] === p2 ? '1' : '2'} won!`;
+    // }
+
+    return p1 === p2 ? 'Draw!': `Player ${rules[p1] === p2 ? '1' : '2'} won!`;
+};
+
+console.log(rps('scissors', 'paper'));
