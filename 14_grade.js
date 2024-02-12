896bb5532e52ec96406002535fd3aed8f42a8f5a
Vicky AS <vickyhuang808@gmail.com>
2024-02-12 23:06:00 +0700

    formatting

--- 14_grade.js
+++ 14_grade.js
@@ -2,8 +2,8 @@
 // Get grade
 
 function getGrade(...s) {
-    const score = s.reduce((acc,current)=> acc + current) / s.length;
-    if (score < 60) return 'F';
+  const score = s.reduce((acc, current) => acc + current) / s.length;
+  if (score < 60) return 'F';
   if (score < 70) return 'D';
   if (score < 80) return 'C';
   if (score < 90) return 'B';
