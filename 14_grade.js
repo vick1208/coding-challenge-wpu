029f08b4b7490972fb848f786da576f83aae5d4b
Vicky AS <vickyhuang808@gmail.com>
2024-02-12 22:51:06 +0700

    day 14

---
+++ 14_grade.js
@@ -0,0 +1,13 @@
+// 14/366
+// Get grade
+
+function getGrade(...s) {
+    const score = s.reduce((acc,current)=> acc + current) / s.length;
+    if (score < 60) return 'F';
+  if (score < 70) return 'D';
+  if (score < 80) return 'C';
+  if (score < 90) return 'B';
+  return 'A';
+}
+
+console.log(getGrade(90, 70, 59));
