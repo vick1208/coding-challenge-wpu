120e32e8a195159150a998679e139f45689983c5
Vicky AS <vickyhuang808@gmail.com>
2024-03-02 22:59:20 +0700

    day 20/366

---
+++ 20_feast.js
@@ -0,0 +1,8 @@
+// 20/366
+// The Feast of Many Beasts
+
+function feast(beast, dish) {
+    return beast.at(0) === dish.at(0) && beast.at(-1) === dish.at(-1)
+}
+
+console.log(feast("great blue heron", "garlic naan"))
